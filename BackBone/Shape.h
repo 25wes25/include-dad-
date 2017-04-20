b04753be345a3edbd821aa13dd5626b8f8a8cc85
SanTransisco <Santransisco@yahoo.com>
2017-04-20 16:35:36 -0700

    Source File folder

---
+++ BackBone/Shape.h
@@ -0,0 +1,67 @@
+enum class ShapeType
+{
+	LINE,
+	POLYLINE,
+	POLYGON,
+	RECTANGLE,
+	ELLIPSE,
+	CIRCLE
+}
+
+enum class Color
+{
+	WHITE,
+	BLACK,
+	RED,
+	GREEN,
+	BLUE,
+	CYAN,
+	MAGENTA,
+	YELLOW,
+	GRAY
+}
+enum class PenStyle
+{
+	NO_PEN,
+	SOLID_LINE,
+	DASH_LINE,
+	DOT_LINE,
+	DASH_DOT_LINE,
+	DASH_DOT_DOT_LINE,
+}
+enum class PenCapStyle
+{
+	FLATCAP,
+	SQUARECAP,
+	ROUNDCAP
+}
+enum class PenJoinStyle
+{
+	MITERJOIN,
+	BEVELJOIN,
+	ROUNDJOIN
+}
+enum class BrushStyle
+{
+	SOLID_PATTERN,
+	HOR_PATTERN,
+	VER_PATTERN,
+	NO_BRUSH
+}
+class Shape
+{
+	private:
+		int id;
+		float penwidth;
+		ShapeType type; 
+		Color penColor;
+		Color brushColor
+		PenStyle style;
+	public:
+		virtual ~Shape()=0;
+		virtual void draw()=0;
+		void setPenJoinStyle(enum PenJoinStyle);
+		void setPenCapStyle(enum PenCapStyle);
+		void setPenStyle(enum PenStyle);
+		void setPenColor(enum Color);
+}
