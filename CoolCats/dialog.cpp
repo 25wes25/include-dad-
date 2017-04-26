e9d4edc18ad059da60cc9da5a3f4405ba27c0607
mulvaneya7 <mulvaneya7@gmail.com>
2017-04-25 17:28:45 -0700

    Add files via upload

--- CoolCats/dialog.cpp
+++ CoolCats/dialog.cpp
@@ -8,6 +8,10 @@ Dialog::Dialog(QWidget *parent) :
     ui->setupUi(this);
     scene = new QGraphicsScene(this);
     ui->graphicsView->setScene(scene);
+    //sets the LAYERS TABLE's WIDTH
+    ui->layerTable->setColumnWidth(0,25);
+    ui->layerTable->setColumnWidth(1,50);
+    ui->layerTable->insertRow(0);
 }
 
 Dialog::~Dialog()
