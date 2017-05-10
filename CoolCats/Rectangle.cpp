cfe5f8b259010b443a75315302eaa1da512b92c1
SanTransisco <Santransisco@yahoo.com>
2017-05-09 20:47:56 -0700

    Add files via upload

---
+++ CoolCats/Rectangle.cpp
@@ -0,0 +1,61 @@
+#include "Rectangle.h"
+#include <QPainter>
+Rectangle::Rectangle():x{25},y{25},width{10},length{10},Shape()
+{
+
+}
+		
+Rectangle::Rectangle(int xIn, int yIn, double w, double l)
+{
+    x=xIn;
+    y=yIn;
+    width = w;
+    length = l;
+}
+
+Rectangle::Rectangle(Rectangle &copy):width{copy.width},length{copy.length}{}
+
+Rectangle::Rectangle(Rectangle &&copy)
+{
+    this->x = copy.x;
+    this->y = copy.y;
+    this->length = copy.length;
+    this->width=copy.width;
+}
+
+Rectangle::~Rectangle()
+{
+    width = 0;
+    length = 0;
+}
+
+void Rectangle::setWidthLength(double w, double l)
+{
+    width = w;
+    length = l;
+}
+
+void Rectangle::setWidth(double w)
+{
+    width = w;
+}
+
+void Rectangle::setLength(double l)
+{
+    length = l;
+}
+void Rectangle::Draw(Canvas *drawArea)
+{
+    QPainter painter(drawArea);
+    painter.save();
+    painter.drawRect(x-width,y-length,width,length);
+    painter.restore();
+}
+bool Rectangle::is_Left_Clicked(QPoint &e)
+{
+    return  (x+width>e.x()&& //Check right side
+             x-width<e.x()&& //Check left side
+             y+length>e.y()&&//Check top
+             y-length<e.y());//CheckBottom
+}
+
---
+++ CoolCats/canvas.h
@@ -0,0 +1,31 @@
+
+#ifndef CANVAS_H
+#define CANVAS_H
+
+#include <QWidget>
+#include "Vector.h"
+
+#include <string>
+#include <QPainter>
+#include <QPoint>
+
+using namespace std;
+class Shape;
+
+class Canvas :public QWidget
+{
+    Q_OBJECT
+public:
+    Canvas(QWidget *parent=0);
+    void addShape(Shape* add);
+    Shape* getCurrentShape(){return currentShape();}
+    void render();
+protected:
+    void paintEvent(QPaintEvent *);
+private:
+    Vector<Shape*> area;
+    Shape* currentShape();
+};
+
+
+#endif // CANVAS_H
